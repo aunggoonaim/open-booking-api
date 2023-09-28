a52c07185cd206501cfac3b40b208237b706212f
aunggoonaim <rabbit.dev.group@gmail.com>
2023-09-29 01:53:23 +0700

    java init project

---
+++ src/test/java/com/aimdevgroup/bookingapi/BookingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimdevgroup.bookingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
